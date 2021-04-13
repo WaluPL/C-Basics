289f7ed3589e62e8e369ea8bc78691304a364386
Michał Walkiewicz <52602306+WaluPL@users.noreply.github.com>
2021-04-13 16:05:51 +0200

    Add files via upload

---
+++ 6/Application.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyUtilityNamespace;
+using Tmp01Namespace;
+
+namespace C05
+{
+    class Application
+    {
+        
+        public void Run()
+        {
+            Main05();
+            MyUtility.Pause("Per aspera ad asperorum ... \n");
+        }
+        private void Main00()
+        {
+            Tmp01 tM = new Tmp01();
+            tM.Print01(); //wywołanie metody nie statycznej wywołane na obiekcie
+            tM.Print02(); 
+            new Tmp01().Print01(); //w locie
+            new Tmp01().Print02();
+
+            Tmp01.Print03(); // na klasie bo statyczne
+            Tmp01.Print04();
+        }
+        delegate void TestDel01(); //funkcja do wywoływania innych funkcji
+        private void Main02()
+        {
+            Tmp01 tM = new Tmp01();
+            TestDel01 myDel = new TestDel01(tM.Print01);// bez nawiasow
+            myDel();
+            Console.WriteLine("-----------------------------");
+            myDel += Tmp01.Print03;
+            myDel += tM.Print01;
+            myDel += tM.Print02;
+            Console.WriteLine("-----------------------------");
+            myDel += tM.Print01;
+            myDel();
+        }
+        private void Main03()
+        {
+            int x = MyUtility.Read();
+            Console.WriteLine("{0} * 2  = {1}", x, Tmp01.ChangeInt(Tmp01.DubInt, x));
+            Console.WriteLine("{0} + 1  = {1}", x, Tmp01.ChangeInt(Tmp01.IncInt, x));
+            Console.WriteLine("{0} - 1  = {1}", x, Tmp01.ChangeInt(Tmp01.DecInt, x));
+        }
+        private void Main04()
+        {
+            int x = MyUtility.Read("x? = ");
+            int y = MyUtility.Read("y? = ");
+            Console.WriteLine("Max({0}, {1}) = {2}", x, y, MyUtility.Extremum(new MyUtility.Question(MyUtility.Max), x, y));
+            Console.WriteLine("Min({0}, {1}) = {2}", x, y, MyUtility.Extremum(new MyUtility.Question(MyUtility.Min), x, y));
+        }
+        private void Main05()
+        {
+            int x = MyUtility.Read("x? = ");
+            int y = MyUtility.Read("y? = ");
+            Console.WriteLine("Max({0}, {1}) = {2}", x, y, MyUtility.Extremum(MyUtility.Max, x, y));
+            Console.WriteLine("Min({0}, {1}) = {2}", x, y, MyUtility.Extremum(MyUtility.Min, x, y));
+        }
+
+    }
+}
+
+
+
+
---
+++ 6/Tmp01.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tmp01Namespace
+{
+    class Tmp01
+    {
+        public void Print01()
+        {
+            Console.WriteLine("Method: Print01()");
+        }
+        public void Print02()
+        {
+            Console.WriteLine("Method: --------------->Print02()");
+        }
+        public static void Print03()
+        {
+            Console.WriteLine("Static Method: Print01()");
+        }
+        public static void Print04()
+        {
+            Console.WriteLine("Static Method ------------> Print01()");
+        }
+        public delegate int OneInt(int x);
+        public static int DubI(int x) { return x * 2; }
+        public static int IncI(int x) { return x + 1; }
+        public static int DecI(int x) { return x - 1; }
+        public static OneInt DubInt= new OneInt(DubI);
+        public static OneInt IncInt= new OneInt(IncI);
+        public static OneInt DecInt= new OneInt(DecI);
+        public static int ChangeInt(OneInt MyC, int x) { return MyC(x); } 
+
+    }
+}
