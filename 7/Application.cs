303a7ec568060114f16b9f980436ea400ad3cb5a
Michał Walkiewicz <52602306+WaluPL@users.noreply.github.com>
2021-04-20 16:01:10 +0200

    Add files via upload

---
+++ 7/Application.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyUtilityNamespace;
+using MyAnamespace;
+
+namespace C07
+{
+    class Application
+    {
+
+        public void Run()
+        {
+            Main01();
+            MyUtility.Pause("Per aspera ad asperorum ... \n");
+        }
+        private void Main00()
+        {
+            MyA myA;
+            myA = new MyA(1, 2, 3); //utworzenie obiektu
+            myA.Print();
+            myA.PrintAll();
+            Console.WriteLine("myA.x2 = {0}", myA.X2); //using getter
+            myA.X2 = 1;
+            myA.X1 = 2;
+            myA.X0 = 3;
+            myA.PrintAll();
+        }
+        
+        private void Main01()
+        {
+            MyA myA0 = new MyA(MyUtility.Read("x2 ?="), MyUtility.Read("x1 ?="), MyUtility.Read("x0 ?="));
+            Console.WriteLine("myA0 : {0})", myA0.ToString());
+             MyA myA1;
+             myA1 = myA0.Copy(); //Copy object
+             myA1.X2 = MyUtility.Read("x2 ?=");
+             Console.WriteLine("myA0 : {0})", myA0);
+             Console.WriteLine("myA1 : {0})", myA1);
+        }
+    }
+}
+
+
+
---
+++ 7/MyA.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAnamespace
+{
+    class MyA
+    {
+        private int x2;
+        private int x1;
+        private int x0;
+        private int cRc;
+        public MyA(int x2, int x1, int x0) //konstruktor klasy MyA
+        {
+            this.x2 = x2; // reference to class object where we are
+            this.x1 = x1;
+            this.x0 = x0;
+            SetCrc();
+        }
+        public MyA Copy() { return new MyA(X2, x1, x0); }
+        public override string ToString()
+        {
+            return "(" + X2 + "," + X1 + "," + X0 + ") -->" + CRC;
+        }
+        public void Print()
+        {
+            Console.WriteLine("({0},{1},{2})", x2, x1, x0);
+        }
+        public void PrintAll()
+        {
+            Console.WriteLine("({0},{1},{2})----->{3}", x2, x1, x0, cRc);
+        }
+        public int X2 //accessors getter and setter for x2
+        {
+            get { return x2; }
+            set { x2 = value;
+                SetCrc();
+            } //set value and reload function
+        }
+        public int X1 //accessors getter and setter for x1
+        {
+            get { return x1; }
+            set { x1 = value;
+                SetCrc();
+            } //set value and reload function
+        }
+        public int X0 //accessors getter and setter
+        {
+            get { return x0; }
+            set {  x0 = value;
+                SetCrc();
+            } //set value and reload function
+        }
+        public int CRC //accessors getter and setter
+        {
+            get { return cRc; }
+
+        }
+        private int CrC()
+        {
+            return  4 * (x2 % 2) +
+                    2 * (x1 % 2) +
+                    1 * (x0 % 2);
+        }
+        private void SetCrc()
+        {
+            cRc = CrC();
+        }
+    }
+}
